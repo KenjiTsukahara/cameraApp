2c80a754d1d4f27bf764ebbdc67b5db145bc419d
塚原健司 <knj0302@tsukaharakenji-no-MacBook-Pro.local>
2016-05-19 13:49:19 +0900

    add files

---
+++ cameraApp/ViewController3.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController3.swift
+//  cameraApp
+//
+//  Created by 塚原健司 on 2016/05/19.
+//  Copyright © 2016年 knj0302. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    @IBAction func backbtn(sender: AnyObject) {
+        
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
